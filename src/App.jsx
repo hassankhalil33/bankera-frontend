80cdb37972878fe77092aea0176a48518ec0e8fd
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2024-01-28 19:47:31 +0200

    Removed Unneccessary Files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Test
+      </p>
+    </div>
+  );
+}
+
+export default App;
