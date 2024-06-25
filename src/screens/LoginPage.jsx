8f9f1bb42f4940ed54d335f016ea4d59510c7cf7
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2024-06-25 16:25:39 +0300

    Stylized Login Page

--- src/screens/LoginPage.jsx
+++ src/screens/LoginPage.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
 import Input from "../components/Input";
+import Button from "../components/Button";
+
+const handleLogin = (e) => {
+  e.preventDefault();
+  console.log("Logged In!");
+};
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
@@ -13,20 +19,30 @@ const LoginPage = () => {
   };
 
   return (
-    <form action="">
-      <h3>Login</h3>
-      <Input
-        value={username}
-        handleChange={handleUserChange}
-        placeholder={"username"}
-      />
-      <Input
-        value={password}
-        handleChange={handlePasswordChange}
-        placeholder={"username"}
-        type="password"
-      />
-    </form>
+    <>
+      <h1>Bankera</h1>
+      <form className="login-form">
+        <div>
+          <h3>Login</h3>
+        </div>
+
+        <Input
+          value={username}
+          handleChange={handleUserChange}
+          placeholder={"Username"}
+        />
+        <Input
+          value={password}
+          handleChange={handlePasswordChange}
+          placeholder={"Password"}
+          type="password"
+        />
+        <Button onPress={handleLogin}>
+          Sign In
+        </Button>
+        <p>not a member? <a href="">sign up</a></p>
+      </form>
+    </>
   )
 };
 
