a306aa56daa3cc88fef7afd6a8ccaf6de8fcf8ac
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2024-06-26 00:55:40 +0300

    Created Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import RegisterPage from './screens/RegisterPage';
 import LoginPage from './screens/LoginPage';
 import Button from './components/Button';
 import Input from './components/Input';
+import HomePage from './screens/HomePage';
 
 const handleClick = () => {
   console.log("clicked!")
@@ -12,7 +13,7 @@ const handleClick = () => {
 function App() {
   return (
     <div className="bg1">
-      <RegisterPage />
+      <HomePage />
     </div>
   );
 };
