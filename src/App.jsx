56ada51efbaa09c5fe655cc59659f68a51d77d8a
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2024-06-27 17:01:36 +0300

    Created Basic Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import "./App.css";
 import RegisterPage from './screens/RegisterPage';
 import LoginPage from './screens/LoginPage';
+import AboutPage from './screens/AboutPage';
+import HomePage from './screens/HomePage';
 import Button from './components/Button';
 import Input from './components/Input';
-import HomePage from './screens/HomePage';
 
 const handleClick = () => {
   console.log("clicked!")
 };
 
+const router = createBrowserRouter([{
+  path: "/",
+  element: <HomePage />
+},
+{
+  path: "/login",
+  element: <LoginPage />
+},
+{
+  path: "/register",
+  element: <RegisterPage />
+},
+{
+  path: "/about",
+  element: <AboutPage />
+}
+]);
+
 function App() {
   return (
     <div className="bg1">
-      <HomePage />
+      <RouterProvider router={router} />
     </div>
   );
 };
