59888ce378b862a7ab0f70b6620f37159f0221f9
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2024-06-28 00:19:45 +0300

    Created Hero Page

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -5,8 +5,15 @@ const HomePage = () => {
   return (
     <>
       <NavBar />
-      <div className="form-container">
-        <h1>This is the Home Page</h1>
+      <div className="home-container">
+        <div className="img-container">
+          <img alt="Bank" className="img" src="./images/bank.jpg" />
+        </div>
+        <h1>Home Of The Banking</h1>
+        <h5>Welcome to Bankera, the one true banking system for all your needs.
+          Our members enjoy alot of features and provisions.
+          So what are you waiting for? Join Now!
+        </h5>
       </div>
     </>
   )
