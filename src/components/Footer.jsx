d1128df231b790504ab66b3d8c28ef77b01dccf3
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2024-06-28 02:43:21 +0300

    Completed Hero Page Styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,20 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>This is the footer.</h1>
-    </div>
+    <>
+      <div className="footer">
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+          </ul>
+          <p>all rights reserved 2024</p>
+        </div>
+        <div>
+          right side
+        </div>
+      </div>
+    </>
   )
 };
 
--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleJoinNow = () => navigate("/register");
+
   return (
     <>
       <NavBar />
       <div className="home-container">
         <div className="img-container">
           <img alt="Bank" className="img" src="./images/bank.jpg" />
+          <h1 id="h1-hero">Home Of The Banking</h1>
+          <h5 id="h5-hero">Welcome to Bankera, the one true banking system for all your needs.
+            Our members enjoy alot of features and provisions. So what are you waiting for?
+          </h5>
+          <div id="btn-index-1">
+            <Button size="large" onPress={handleJoinNow}>Join Now!</Button>
+          </div>
         </div>
-        <h1>Home Of The Banking</h1>
-        <h5>Welcome to Bankera, the one true banking system for all your needs.
-          Our members enjoy alot of features and provisions.
-          So what are you waiting for? Join Now!
-        </h5>
       </div>
       <Footer />
     </>
