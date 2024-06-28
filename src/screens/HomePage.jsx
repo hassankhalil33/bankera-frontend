ee516babef2ab613eddf70e594641db7f9d440a1
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2024-06-28 21:44:37 +0300

    Added Home Page Content

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -24,6 +24,38 @@ const HomePage = () => {
           </div>
         </div>
       </div>
+      <div className="content-container">
+        <div>
+          <h5>Simply hit send</h5>
+          <p>Sending and receiving money is just as easy as texting a friend.</p>
+          <Button size="large" onPress={handleJoinNow}>Try it out</Button>
+        </div>
+        <div>
+          <img className="img-ctn1" src="./images/send.png" alt="send" />
+        </div>
+      </div>
+      <div className="content-container">
+        <div>
+          <img className="img-ctn2" src="./images/globe.png" alt="globe" />
+        </div>
+        <div>
+          <h5>And send across the globe too</h5>
+          <p>Whether it’s Morocco or Mexico, you can transfer there — really,
+            really fast. Oh, and the rates are as good as you’ve heard.</p>
+          <Button size="large" onPress={handleJoinNow}>See how to send it far</Button>
+        </div>
+      </div>
+      <div className="content-container-last">
+        <div>
+          <img className="img-ctn1" src="./images/security.png" alt="security" />
+        </div>
+        <div>
+          <h5>This is money security</h5>
+          <p>Welcome to your money’s new home — protected round-the-clock by Revolut Secure’s proactive,
+            purpose-built defences. This is what a new era of money security looks like.</p>
+          <Button size="large" onPress={handleJoinNow}>Tell me more</Button>
+        </div>
+      </div>
       <Footer />
     </>
   )
