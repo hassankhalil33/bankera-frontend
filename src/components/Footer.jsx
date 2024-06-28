19abf9ac15f9883ad55035ec354ddd923b4547ab
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2024-06-29 02:19:37 +0300

    Stylized Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <>
       <div className="footer">
-        <div>
+        <div className="footer-div">
           <ul>
-            <li>Home</li>
-            <li>About</li>
+            <li className="footer-li">
+              <Link className="footer-link" to={"/"}>
+                Home
+              </Link>
+            </li>
+            <li className="footer-li">
+              <Link className="footer-link" to={"/about"}>
+                About
+              </Link>
+            </li>
+            <li className="footer-li">Legal</li>
+            <li className="footer-li">Contact Us</li>
           </ul>
-          <p>all rights reserved 2024</p>
+          <p>©<span id="logo-footer">Bankera</span> all rights reserved 2024</p>
         </div>
-        <div>
-          right side
+        <div className="footer-div2">
+          <img className="img-footer" src="./images/linkedin.png" alt="linkedin" />
+          <img className="img-footer" src="./images/instagram.png" alt="instagram" />
+          <img className="img-footer" src="./images/facebook.png" alt="facebook" />
         </div>
       </div>
     </>
